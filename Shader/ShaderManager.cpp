9de2e31e46ec7e3e1ed1702434c97bfe534d913d
shlomon53 <yuusuke6t@outlook.jp>
2022-04-01 12:10:28 +0900

    transなんちゃらを使わない方針

--- Shader/ShaderManager.cpp
+++ Shader/ShaderManager.cpp
@@ -1,11 +1,11 @@
 #include"ShaderManager.h"
 
 HLSLShader ShaderManager::playerShader;
-HLSLShader ShaderManager::translucentShader;
+//HLSLShader ShaderManager::translucentShader;
 void ShaderManager::LoadShaders()
 {
 	playerShader.vsBlob = LoadShader(L"Resource/HLSL/OBJVertexShader.hlsl", "vs_5_0");
 	playerShader.psBlob = LoadShader(L"Resource/HLSL/OBJPixelShader.hlsl", "ps_5_0");
-	translucentShader.vsBlob = LoadShader(L"Resource/HLSL/OBJVertexShader.hlsl", "vs_5_0");
-	translucentShader.psBlob = LoadShader(L"Resource/HLSL/transluentPixelShader.hlsl", "ps_5_0");
+	/*translucentShader.vsBlob = LoadShader(L"Resource/HLSL/OBJVertexShader.hlsl", "vs_5_0");
+	translucentShader.psBlob = LoadShader(L"Resource/HLSL/transluentPixelShader.hlsl", "ps_5_0");*/
 }
